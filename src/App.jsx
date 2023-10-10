4e3218bf056775a8d497a5af7ac8a0b879fedc87
Alexandros Polymeros <alpolymer@gmail.com>
2023-10-10 19:39:04 +0300

    friends list

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,47 @@ const initialFriends = [
 ]
 function App() {
   return (
-    <>
-      <h1>Eat `n`Split</h1>
-    </>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
   )
 }
 
 export default App
+
+function FriendsList() {
+  const friends = initialFriends
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  )
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance > 0 && (
+        <p className="green">
+          You owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance < 0 && (
+        <p className="red">
+          {friend.name} owes you{"  "}
+          {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even </p>}
+
+      <button className="button">Select</button>
+    </li>
+  )
+}
